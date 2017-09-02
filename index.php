a666eb24ca8efa6f812bb7c828ea81f12d38b55e
thetongz <miiz_tongz@live.com>
2017-09-02 11:09:43 +0700

    add index page

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <title>Tea Time Shop</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="static/fontawesome/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="static/style/bootstrap.min.css">
+    <script src="static/javascript/jquery.min.js"></script>
+    <script src="static/javascript/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Tea Time</a>
+        </div>
+        <div>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Product</a></li>
+                <li><a href="#">Sign in</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container">
+    <div class="jumbotron">
+        <h1>Tea Time <i class="fa fa-coffee" aria-hidden="true"></i></h1>
+        <p>A cup of <b><i>TEA</i></b> make everything <b><i>BETTER</i></b></p>
+        <p><a class="btn btn-primary btn-lg">View our product</a></p>
+    </div>
+</div>
+</body>
+</html>
