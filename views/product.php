53e420d569ba0d29d9ad281271abf62da45990c9
thetongz <miiz_tongz@live.com>
2017-09-03 14:09:53 +0700

    implement delete product

--- views/product.php
+++ views/product.php
@@ -3,8 +3,21 @@
 session_start();
 
 require "../controllers/product.controller.php";
+require "../utilities/redirect.php";
 
 $products = getAllProduct();
+
+if (isset($_POST['delete'])) {
+    $index = $_POST['delete'];
+    $productID = $products[$index]['id'];
+
+    $deleteStatus = deleteProduct($productID);
+    if($deleteStatus == true) {
+        redirect("product");
+    } else {
+        echo '<script>alert("Delete incomplete")</script>';
+    }
+}
 
 ?>
 <head>
@@ -15,6 +28,12 @@ $products = getAllProduct();
     <link rel="stylesheet" href="static/style/bootstrap.min.css">
     <script src="static/javascript/jquery.min.js"></script>
     <script src="static/javascript/bootstrap.min.js"></script>
+    <style>
+        .icon-button {
+            background-color: transparent;
+            border: none;
+        }
+    </style>
 </head>
 <body>
 <nav class="navbar navbar-default">
@@ -30,7 +49,7 @@ $products = getAllProduct();
                 }
                 ?>
                 <li class="active">
-                    <a href="#">Product</a>
+                    <a href="product">Product</a>
                 </li>
                 <?php
                 if (!isset($_SESSION['username'])) {
@@ -45,42 +64,52 @@ $products = getAllProduct();
 </nav>
 <div class="container">
     <div>
-        <h1>*** items found</h1>
+        <h1><?php echo count($products) . " products available" ?></h1>
         <table class="table table-striped table-hover ">
             <thead>
             <tr>
                 <th width="5%">#</th>
                 <th width="20%">Image</th>
-                <th width="15%">Name</th>
+                <th width="14%">Name</th>
                 <th width="33%">Description</th>
                 <th width="10%">Price</th>
                 <th width="10%">Amount</th>
-                <th width="7%">Other</th>
+                <th width="8%">Other</th>
             </tr>
             </thead>
             <tbody>
             <tr>
                 <?php
-                    foreach($products as $product) {
-                        echo '
-                            <td>'. $product["id"]. '</td>
+                foreach ($products as $index=>$product) {
+                    echo '<tr>
+                            <td>' . $product["id"] . '</td>
                             <td>
-                                <img src="'. $product["imageURL"].'" width="160px" height="160px">
+                                <img src="' . $product["imageURL"] . '" width="160px" height="160px">
                             </td>
-                            <td>'. $product["name"]. '</td>
-                            <td>'. $product["description"]. '</td>
-                            <td>'. $product["price"]. '</td>
-                            <td>'. $product["amount"]. '</td>
+                            <td>' . $product["name"] . '</td>
+                            <td>' . $product["description"] . '</td>
+                            <td>' . $product["price"] . '</td>
+                            <td>' . $product["amount"] . '</td>
                             <td>';
 
-                        if (isset($_SESSION['username'])) {
-                            echo '<i class="fa fa-pencil fa-2x" aria-hidden="true"></i>
-                                        &nbsp;
-                                  <i class="fa fa-trash-o fa-2x" aria-hidden="true"></i>';
-                        }
+                    if (isset($_SESSION['username'])) { ?>
+                        <form method="POST" action=''>
+                            <div>
+                                <button type="submit" name="edit"
+                                        value=<?php echo $index; ?> class="icon-button">
+                                    <i class="fa fa-pencil fa-2x" aria-hidden="true"></i>
+                                </button>
+                                &nbsp;
+                                <button type="submit" name="delete" onclick="return confirm('Are you sure?')"
+                                        value=<?php echo $index; ?> class="icon-button">
+                                    <i class="fa fa-trash-o fa-2x" aria-hidden="true"></i>
+                                </button>
+                            </div>
+                        </form>
+                    <?php }
 
-                        echo '</td>';
-                    }
+                    echo '</td></<tr>';
+                }
                 ?>
             </tr>
             </tbody>
