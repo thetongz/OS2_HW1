ec3fd146a2bfe121e0cda4424d1976913eb99cac
thetongz <miiz_tongz@live.com>
2017-10-03 15:40:03 +0700

    use S3Bucket instance of local storage

--- controllers/upload.controller.php
+++ controllers/upload.controller.php
@@ -1,23 +1,49 @@
 <?php
 
+use Aws\S3\Exception\S3Exception;
+
+require "../configs/s3.config.php";
+
 class UploadController {
     private $uploadImagePath;
-    private $displayImagePath;
+    private $s3;
+    private $s3Client;
+    private $s3Bucket;
 
     function __construct() {
         $this->uploadImagePath = "../static/images";
-        $this->displayImagePath = "static/images";
+        $this->s3 = new S3();
+        $this->s3Client = $this->s3->createS3Client();
+        $this->s3Bucket = $this->s3->getS3Bucket();
     }
 
     function uploadImage($name, $tempName) {
         $tempFilePath = "{$this->uploadImagePath}/{$name}";
         move_uploaded_file($tempName, $tempFilePath);
 
-        return "{$this->displayImagePath}/{$name}";
+        $this->putObjectToS3($name, $tempFilePath);
+
+        return $this->getUploadedImageUrl($name);
     }
 
-    function removeUploadImage($path) {
-        unlink("../". $path);
+    function putObjectToS3($key, $filePath) {
+        try {
+            $this->s3Client->putObject([
+                'Bucket' => $this->s3Bucket,
+                'Key' => $key,
+                'Body' => fopen($filePath, 'rb'),
+                'ACL' => 'public-read'
+            ]);
+
+            unlink($filePath);
+
+        } catch (S3Exception $e) {
+            echo $e;
+        } 
+    }
+
+    function getUploadedImageUrl($key) {
+        return "https://s3-ap-southeast-1.amazonaws.com/{$this->s3Bucket}/{$key}";
     }
 }
 ?>
